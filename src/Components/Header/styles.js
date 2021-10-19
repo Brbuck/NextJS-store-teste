1577e248beb72381bcf2b286ea12f8f5a8abc307
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-10-19 19:51:15 -0300

    Update Header styles.js

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -3,8 +3,32 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100px;
-    background-color: #666;
-    display: flex;
-    align-items: center;
     background-image: linear-gradient(160deg, #ADFF2F , #AB82FF);
 `;
+
+export const Navbar = styled.nav`
+    width: 100%;
+    max-width: 1300px;
+    height: 50px;
+    padding: 5px 15px;
+    margin: -25px auto 0px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FF6A6A;
+
+    >a{
+        color: #555;
+        padding: 12px;
+        margin-left: 15px;
+
+        font-size: 1.2rem;
+        font-weight: 500;
+
+        &:hover{
+            color: #111;
+        }
+    }
+
+`
