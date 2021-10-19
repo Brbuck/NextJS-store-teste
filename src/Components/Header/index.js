15d6a586f39242bee79c3ea9ad33b16c28143f82
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-10-19 19:50:28 -0300

    Update Header index.js

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,19 @@
-import {Container} from './styles'
+import Link from 'next/link';
+import { Container, Navbar } from './styles'
 
 
 export default function Header() {
 
     return (
-        <Container>
-            <p>Teste</p>
-        </Container>
+        <>
+            <Container />
+            <Navbar>
+                <Link href='/'><a>Home</a></Link>
+                <Link href='blush'><a>Blush</a></Link>
+                <Link href='bronzer'><a>Bronzer</a></Link>
+                <Link href='eyebrow'><a>Eyebrow</a></Link>
+            </Navbar>
+
+        </>
     )
 }
