f8a39511f34fd0faf39ff339e2b4cc6d7594c04f
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-10-19 19:48:40 -0300

    Create bronzer.js

---
+++ src/pages/bronzer.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import {Container, Wrapper} from '../styles/showCase'
+
+
+export default function Blush({ data }) {
+    return (
+        <Wrapper>
+            {data.map((item, index) => (
+                <Link key={index} href={`/products/${item.id}`}>
+                    <Container>
+                        <img src={item.image_link} alt={item.name} width="120px" />
+                        <h3>{item.brand}</h3>
+                        <span>{item.name}</span>
+                        <span>R$ {item.price}</span>
+                    </Container>
+                </Link>
+            ))}
+
+        </Wrapper>
+    );
+};
+
+export async function getStaticProps() {
+    const response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json?product_type=bronzer')
+    const data = await response.json()
+
+    return {
+        props: {
+            data,
+        },
+        revalidate: 10,
+    }
+}
